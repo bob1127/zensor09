65a26ba7c4802ca81fbfdd752f81499507b4441a
bob1127 <61307295+bob1127@users.noreply.github.com>
2021-04-13 13:35:31 +0800

    second

--- src/js/product_detail.js
+++ src/js/product_detail.js
@@ -3,8 +3,10 @@ import '../css/product_detail.scss';
 import '../css/style.scss';
 import AOS from 'aos';
 import 'aos/dist/aos.css'; // You can also use <link> 
-import Swiper from 'swiper';
+
 import 'swiper/swiper-bundle.css';
+
+
 
 AOS.init();
 
