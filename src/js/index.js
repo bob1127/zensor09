c048cbeb8b0a05ace8c25942e77cf02438f3d228
bob1127 <61307295+bob1127@users.noreply.github.com>
2021-04-28 14:20:07 +0800

    ee

--- src/js/index.js
+++ src/js/index.js
@@ -7,7 +7,7 @@ import 'aos/dist/aos.css'; // You can also use <link> for styles
 
 AOS.init();
 
-
+import gsap from "gsap";
 import $ from 'jquery';
 import 'slick-carousel';
 
@@ -31,6 +31,34 @@ const logEvent = (eventName, element) => {
 
 var jQuery = $
 
+const open = document.querySelector('.container');
+const close = document.querySelector('.close_nav');
+var tl = gsap.timeline({ defaults: { duration: 1, ease: 'expo.inOut' } });
+open.addEventListener('click', () => {
+  if (tl.reversed()) {
+    tl.play();
+  } else {
+    tl.to('nav', { right: 0 })
+      .to('nav', { height: '100vh' }, '-=.1')
+      .to('nav ul li a', { opacity: 1, pointerEvents: 'all', stagger: .05 }, '-=.8')
+      .to('.close_nav', { opacity: 1, pointerEvents: 'all' }, "-=.8")
+      .to('nav h2', { opacity: 1 }, '-=.1');
+  }
+});
+
+close.addEventListener('click', () => {
+  tl.reverse();
+});
+$('.bars').click(function(){
+  $('.bars').hide(500)
+})
+
+$('.bars').click(function(){
+  $('.mobile_nav ul').css('visibility','visible')
+})
+$('.mobile_nav_toggle').click(function(){
+  $('.bars').delay(3000).show(500)
+})
 
 
 
