036ba05cbaf88e8284a0b10e64e0998fb59c61fa
bob1127 <61307295+bob1127@users.noreply.github.com>
2021-04-28 17:10:58 +0800

    re

--- src/js/article.js
+++ src/js/article.js
@@ -81,6 +81,7 @@ AOS.init();
         if (tl.reversed()) {
           tl.play();
         } else {
+          
           tl.to('nav', { right: 0 })
             .to('nav', { height: '100vh' }, '-=.1')
             .to('nav ul li a', { opacity: 1, pointerEvents: 'all', stagger: .05 }, '-=.8')
