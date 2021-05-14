ccd03a26bf5406bb5f83bf822efc6fa50e069329
bob1127 <61307295+bob1127@users.noreply.github.com>
2021-05-14 17:45:12 +0800

    dfdsf

--- src/js/index.js
+++ src/js/index.js
@@ -12,6 +12,26 @@ import $ from 'jquery';
 import 'slick-carousel';
 
 import LazyLoad from "vanilla-lazyload";
+
+const header = $('.header');
+	const icon = $('.icon-container');
+	icon.onclick = function () {
+		header.classList.toggle('menu-open');
+    $('.cart-mobile-icon').fadeToggle()
+	}
+  $('.menu-item-product a').click(function(){
+    $('.menu-ul').slideToggle(500)
+  })
+
+  $('.menu-wireless').click(function(){
+    $('.menu-inside-wireless').slideToggle(500)
+  })
+$('.menu-simulator').click(function(){
+    $('.menu-inside-simulator').slideToggle(500)
+  })
+ 
+
+
 $('#policy-btn').click(function(){
   $('.policy').fadeOut();
 })  
@@ -36,24 +56,7 @@ const logEvent = (eventName, element) => {
 
 var jQuery = $
 
-const open = document.querySelector('.container');
-const close = document.querySelector('.close_mobile');
-var tl = gsap.timeline({ defaults: { duration: 1, ease: 'expo.inOut' } });
-open.addEventListener('click', () => {
-  if (tl.reversed()) {
-    tl.play();
-  } else {
-    tl.to('nav', { right: 0 })
-      .to('nav', { height: '100vh' }, '-=.1')
-      .to('nav ul li a', { opacity: 1, pointerEvents: 'all', stagger: .05 }, '-=.8')
-      .to('.close_mobile', { opacity: 1, pointerEvents: 'all' }, "-=.8")
-      .to('nav h2', { opacity: 1 }, '-=.1');
-  }
-});
 
-close.addEventListener('click', () => {
-  tl.reverse();
-});
 $('.bars').click(function(){
   $('.bars').hide(500)
 })
