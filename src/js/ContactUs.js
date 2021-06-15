0777ba1e43e00d509b441207fcfdfa10f8b98bf7
bob1127 <61307295+bob1127@users.noreply.github.com>
2021-06-15 17:02:18 +0800

    jnj

--- src/js/ContactUs.js
+++ src/js/ContactUs.js
@@ -18,26 +18,26 @@ icon.onclick = function () {
   $('.icon-fit-cart').fadeToggle()
   $('.logo img').fadeToggle()
 }
-$('.menu-item-product a').click(function(){
+$('.menu-item-product a').click(function () {
   $('.menu-ul').slideToggle(500)
 })
 
-$('.menu-wireless').click(function(){
+$('.menu-wireless').click(function () {
   $('.menu-inside-wireless').slideToggle(500)
 })
-$('.menu-simulator').click(function(){
+$('.menu-simulator').click(function () {
   $('.menu-inside-simulator').slideToggle(500)
 })
 
 
 
-$('.mobile_product').click(function(){
+$('.mobile_product').click(function () {
   $('.inside_ul').animate({
-    'visibility':'visible'
-  },500)
+    'visibility': 'visible'
+  }, 500)
 })
 
-$('#product_toggle').click(function(){
+$('#product_toggle').click(function () {
   $('.inside_product_nav ').slideToggle(500)
 })
 
@@ -45,86 +45,85 @@ $('#product_toggle').click(function(){
 
 var mediaQuery = window.matchMedia("(max-width:1100px)");
 
-    $(document).ready(function () {
-      var stickyNavTop = $("nav").offset().top;
+$(document).ready(function () {
+  var stickyNavTop = $("nav").offset().top;
 
-      var stickyNav = function () {
-        var scrollTop = $(window).scrollTop();
+  var stickyNav = function () {
+    var scrollTop = $(window).scrollTop();
 
-        if (scrollTop > stickyNavTop) {
-          $("nav").addClass("sticky");
+    if (scrollTop > stickyNavTop) {
+      $("nav").addClass("sticky");
 
-          $("#nav_product").hover(function () {
-            $(".product_toggle").css("top", "11.5vh");
-          });
-        } else {
-          $("nav").removeClass("sticky");
+      $("#nav_product").hover(function () {
+        $(".product_toggle").css("top", "11.5vh");
+      });
+    } else {
+      $("nav").removeClass("sticky");
 
-          $("#nav_product").hover(function () {
-            $(".product_toggle").css("top", "11.5vh");
-            if (mediaQuery.matches) {
-              $(".product_toggle").css("top", "7vh");
-            }
-          });
+      $("#nav_product").hover(function () {
+        $(".product_toggle").css("top", "11.5vh");
+        if (mediaQuery.matches) {
+          $(".product_toggle").css("top", "7vh");
         }
-      };
-
-      stickyNav();
-
-      $(window).scroll(function () {
-        stickyNav();
       });
-    });
-    //top_fix_nav  virticle carousel
-    $(document).ready(function () {
-      $(".top_fix_nav").slick({
-        vertical: true,
-        dots: false,
-        autoplay: true,
-      });
-    });
+    }
+  };
 
-     //chage lang 
+  stickyNav();
+
+  $(window).scroll(function () {
+    stickyNav();
+  });
+});
+//top_fix_nav  virticle carousel
+$(document).ready(function () {
+  $(".top_fix_nav").slick({
+    vertical: true,
+    dots: false,
+    autoplay: true,
+  });
+});
+
+//chage lang 
 $('.icon-earth').hover(function () {
   $('.toggle-menu').fadeIn(500)
 })
 
-     //form
+//form
 
-     var form = document.getElementById("my-form");
-     var form02 = document.getElementById("my-form02");
-
-     
-         
-         async function handleSubmit(event) {
-           event.preventDefault();
-           var status = document.getElementById("my-form-status");
-           var data = new FormData(event.target);
-           fetch(event.target.action, {
-             method: form.method,
-             body: data,
-             headers: {
-                 'Accept': 'application/json'
-             }
-           }).then(response => {
-             alert('成功寄出！')
-             form.reset()
-             form02.reset()
-       
-           }).catch(error => {
-             status.innerHTML = "Oops! There was a problem submitting your form"
-           });
-         }
-         form.addEventListener("submit", handleSubmit)
-         form02.addEventListener("submit", handleSubmit)
-       
-     
-     
+var form = document.getElementById("my-form");
+var form02 = document.getElementById("my-form02");
 
 
- 
 
-    
+async function handleSubmit(event) {
+  event.preventDefault();
+  var status = document.getElementById("my-form-status");
+  var data = new FormData(event.target);
+  fetch(event.target.action, {
+    method: form.method,
+    body: data,
+    headers: {
+      'Accept': 'application/json'
+    }
+  }).then(response => {
+    alert('成功寄出！')
+    form.reset()
+    form02.reset()
 
-      
-      
+  }).catch(error => {
+    status.innerHTML = "Oops! There was a problem submitting your form"
+  });
+}
+form.addEventListener("submit", handleSubmit)
+form02.addEventListener("submit", handleSubmit)
+
+
+
+
+
+
+
+
+
+
--- src/js/ProductCatagory.js
+++ src/js/ProductCatagory.js
@@ -9,31 +9,24 @@ import 'swiper/swiper-bundle.css';
 
 
 
-
-//chage lang 
-$('.icon-earth').hover(function () {
-  $('.toggle-menu').fadeIn(500)
-})
 //mobile_nav
- const header = document.querySelector('.header');
-	const icon = document.querySelector('.icon-container');
-	icon.onclick = function () {
-		header.classList.toggle('menu-open');
-    $('.icon-fit-cart').fadeToggle()
-    $('.logo img').fadeToggle()
-	}
-  $('.menu-item-product a').click(function(){
-    $('.menu-ul').slideToggle(500)
-  })
+const header = document.querySelector('.header');
+const icon = document.querySelector('.icon-container');
+icon.onclick = function () {
+  header.classList.toggle('menu-open');
+  $('.icon-fit-cart').fadeToggle()
+  $('.logo img').fadeToggle()
+}
+$('.menu-item-product a').click(function(){
+  $('.menu-ul').slideToggle(500)
+})
 
-  $('.menu-wireless').click(function(){
-    $('.menu-inside-wireless').slideToggle(500)
-  })
+$('.menu-wireless').click(function(){
+  $('.menu-inside-wireless').slideToggle(500)
+})
 $('.menu-simulator').click(function(){
-    $('.menu-inside-simulator').slideToggle(500)
-  })
- 
-
+  $('.menu-inside-simulator').slideToggle(500)
+})
 
 
 
@@ -47,64 +40,83 @@ $('#product_toggle').click(function(){
   $('.inside_product_nav ').slideToggle(500)
 })
 
-    //topnav close
-    $('.topFix_nav_close').click(function(){
-        $('.top_fix_nav').slideUp();
-      })
-    
-      
-
-   
-//      const quotes = document.querySelectorAll(".quote");
 
 
-// ScrollTrigger.addEventListener("refresh", setupSplits);
-// setupSplits();
-        
+var mediaQuery = window.matchMedia("(max-width:1100px)");
 
-      var mediaQuery = window.matchMedia('(max-width:1100px)')
-      
-      
-      
-      $(document).ready(function () {
-        var stickyNavTop = $("nav").offset().top;
+    $(document).ready(function () {
+      var stickyNavTop = $("nav").offset().top;
 
-        var stickyNav = function () {
-          var scrollTop = $(window).scrollTop();
+      var stickyNav = function () {
+        var scrollTop = $(window).scrollTop();
 
-          if (scrollTop > stickyNavTop) {
-            $("nav").addClass("sticky");
-          
-            $('#nav_product').hover(function(){
-              $('.product_toggle').css('top','11.5vh')
-            })
-          } else {
-            $("nav").removeClass("sticky");
-            
-            $('#nav_product').hover(function(){
-              $('.product_toggle').css('top','11.5vh')
-              if(mediaQuery.matches){
-                $('.product_toggle').css('top','7vh')
-          }
-            })
-          }
-        };
-        
+        if (scrollTop > stickyNavTop) {
+          $("nav").addClass("sticky");
+
+          $("#nav_product").hover(function () {
+            $(".product_toggle").css("top", "11.5vh");
+          });
+        } else {
+          $("nav").removeClass("sticky");
+
+          $("#nav_product").hover(function () {
+            $(".product_toggle").css("top", "11.5vh");
+            if (mediaQuery.matches) {
+              $(".product_toggle").css("top", "7vh");
+            }
+          });
+        }
+      };
+
+      stickyNav();
+
+      $(window).scroll(function () {
         stickyNav();
-
-        $(window).scroll(function () {
-          stickyNav();
-        });
       });
-      //top_fix_nav  virticle carousel
-      $(document).ready(function () {
-        $(".top_fix_nav").slick({
-          vertical: true,
-          dots: false,
-          autoplay: true,
-        });
+    });
+    //top_fix_nav  virticle carousel
+    $(document).ready(function () {
+      $(".top_fix_nav").slick({
+        vertical: true,
+        dots: false,
+        autoplay: true,
       });
+    });
 
+     //chage lang 
+$('.icon-earth').hover(function () {
+  $('.toggle-menu').fadeIn(500)
+})
+
+     //form
+
+     var form = document.getElementById("my-form");
+     var form02 = document.getElementById("my-form02");
+
+     
+         
+         async function handleSubmit(event) {
+           event.preventDefault();
+           var status = document.getElementById("my-form-status");
+           var data = new FormData(event.target);
+           fetch(event.target.action, {
+             method: form.method,
+             body: data,
+             headers: {
+                 'Accept': 'application/json'
+             }
+           }).then(response => {
+             alert('成功寄出！')
+             form.reset()
+             form02.reset()
+       
+           }).catch(error => {
+             status.innerHTML = "Oops! There was a problem submitting your form"
+           });
+         }
+         form.addEventListener("submit", handleSubmit)
+         form02.addEventListener("submit", handleSubmit)
+       
 
 
    $(function () {
