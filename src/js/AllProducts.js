e56aaa0e5501ceca8148935ddacf5108b88020e3
bob1127 <61307295+bob1127@users.noreply.github.com>
2021-08-10 16:54:44 +0800

    secontt

--- src/js/AllProducts.js
+++ src/js/AllProducts.js
@@ -26,6 +26,16 @@ $('#gotop img').click(function(){
     scrollTop: 0
   },600)
 })
+
+$(window).scroll(function(){
+  if($(this).scrollTop()>700){
+    $('#mobile_buttons').fadeIn(500)
+  }else(
+    $('#mobile_buttons').fadeOut(500)
+  )
+})
+
+
 $(function() {
   $('.popup-youtube, .popup-vimeo').magnificPopup({
       disableOn: 700,
